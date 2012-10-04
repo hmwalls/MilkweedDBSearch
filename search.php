665dcbf2d0bf02552207b6eaa101bacfbcb97a49
Hailey Walls <hailey.m.w@gmail.com>
2012-10-04 16:48:12 -0700

    can now search by plant database code

--- search.php
+++ search.php
@@ -8,20 +8,25 @@
     <h3>Search</h3>
     <p>You  may search either by first or last name</p>
     <form  method="post" action="search.php?go"  id="searchform">
-      <input  type="text" name="name">
-      <input  type="submit" name="submit" value="Search">
+      <input  type="text" name="databasecodeentry">
+      <input  type="submit" name="submit" value="Search by Plant Database Code">
     </form>
+    <form  method="post" action="search.php?go"  id="searchform">
+      <input  type="text" name="stateentry">
+      <input  type="submit" name="submit" value="Search State">
+    </form>
+
   <?php
   if(isset($_POST['submit'])){
   if(isset($_GET['go'])){
-  if(preg_match("/^[  a-zA-Z]+/", $_POST['name'])){
-  $name=$_POST['name'];
+  if(preg_match("/^[  a-zA-Z0-9]+/", $_POST['databasecodeentry'])){
+  $databasecodeentry=$_POST['databasecodeentry'];
   //connect  to the database
   $db=mysql_connect  ("localhost", "hailey",  "b0Mbu$") or die ('I cannot connect to the database  because: ' . mysql_error());
   //-select  the database to use
   $mydb=mysql_select_db("milkweed");
   //-query  the database table
-  $sql="Select Commonname, scientificname, databasecode, name, state, zip, url, email, phone, notes, seed, liveplant FROM availability JOIN plants ON availability.plant_ID = plants.plant_ID Join sources ON availability.source_ID = sources.source_ID;";
+  $sql="Select Commonname, scientificname, databasecode, name, state, zip, url, email, phone, notes, seed, liveplant FROM availability JOIN plants ON availability.plant_ID = plants.plant_ID Join sources ON availability.source_ID = sources.source_ID WHERE databasecode like '$databasecodeentry%'";
   //-run  the query against the mysql query function
   $result=mysql_query($sql);
   //-create  while loop and loop through result set
@@ -42,6 +47,7 @@
   echo "<table border=1>\n";
   echo "<tr> <td> $liveplant </td> <td> $seed </td> <td> $Commonname </td> <td> $scientificname </td> <td> $databasecode </td> <td> $name </td> <td> $state </td> <td> $zip </td> <td> $url </td> <td> $email </td> <td> $phone </td> <td> $notes </td> </tr>\n";
   echo "</table>";
+  echo "$databasecodeentry";
   }
   }
   else{
