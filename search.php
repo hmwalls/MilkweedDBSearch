fa14c2eda7fe24af71589fdcf59021e52c4647a9
Hailey Walls <hailey.m.w@gmail.com>
2012-10-16 12:35:09 -0700

    Revert "fixed issues with searching w/out code, w/zip"
    This reverts commit b2e97d423309de8ce69693a423f30912a705c729.

--- search.php
+++ search.php
@@ -1,16 +1,16 @@
 <html>
   <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-    <title>Search Contacts</title>
+    <meta  http-equiv="Content-Type" content="text/html;  charset=iso-8859-1">
+    <title>Search  Contacts</title>
   </head>
 
   <body>
     <h3>Search For Native Milkweed</h3>
-    <form method="post" action="search.php?go" id="searchform"> <br>
-      Plant Database Code: <input type="text" name="databasecodeentry"><br>
-      Common Name: <input type="text" name="commonnameentry"><br>
-      Scientific Name: <input type="text" name="scientificnameentry"><br>
-      ZIP Code: <input type="text" name="zipcodeentry"><br>
+    <form  method="post" action="search.php?go"  id="searchform"> <br>
+      Plant Database Code: <input  type="text" name="databasecodeentry"><br>
+      Common Name: <input  type="text" name="commonnameentry"><br>
+      Scientific Name: <input  type="text" name="scientificnameentry"><br>
+      ZIP Code: <input  type="text" name="zipcodeentry"><br>
       State: <select name="stateentry">
         <option value="">Any State</option>
         <option value="AL">Alabama</option>
@@ -63,13 +63,13 @@
         <option value="WA">Washington</option>
         <option value="WV">West Virginia</option>
         <option value="WI">Wisconsin</option>
-        <option value="WY">Wyoming</option>
+        <option value="WY">Wyoming</option> 
       </select> <br>
-      <input type="submit" name="submit" value="Search">
+      <input  type="submit" name="submit" value="Search">
     </form>
 
     <?php
-      $databasecodeentry = $_POST['databasecodeentry'];
+      $databasecodeentry = $_POST['databasecodeentry'];  
       $commonnameentry = $_POST['commonnameentry'];
       $scientificnameentry = $_POST['scientificnameentry'];
       $stateentry = $_POST['stateentry'];
@@ -78,11 +78,10 @@
       $liveplantentry = $_POST['liveplantentry'];
       if (isset($_POST['submit'])){
         if(isset($_GET['go'])){
-          $db=mysql_connect ("localhost", "hailey", "b0Mbu$") or die ('I cannot connect to the database because: ' . mysql_error());
 
-          $mydb=mysql_select_db("milkweed");
+          $mysqli = new mysqli('localhost', 'hailey', 'b0Mbu$', 'milkweed');
 
-          $sql="SELECT commonname, scientificname, databasecode, name, state, zip, url, email, phone, notes, seed, liveplant 
+          $query="SELECT commonname, scientificname, databasecode, name, state, zip, url, email, phone, notes, seed, liveplant 
           FROM availability 
           JOIN plants 
           ON availability.plant_ID = plants.plant_ID 
@@ -92,58 +91,45 @@
           AND databasecode LIKE '$databasecodeentry%' 
           AND commonname LIKE '$commonnameentry%' 
           AND scientificname LIKE '$scientificnameentry%' 
-          AND zip LIKE '$zipentry%'";
+          AND zip LIKE'$zipentry%'";
 
-          $result=mysql_query($sql);
+          $result=$mysqli->query($query);
             echo "<table border=1 width=80%>
-                  <tr width=80%>
-                  <th width=8.3%> Live Plants Available? </th>
-                  <th width=8.3%> Seeds Available? </th>
-                  <th width=8.3%> Common Name </th>
-                  <th width=8.3%> Scientific Name </th>
-                  <th width=8.3%> Plant Database Code </th>
-                  <th width=8.3%> Source Name </th>
-                  <th width=8.3%> State </th>
-                  <th width=8.3%> ZIP </th>
-                  <th width=8.3%> URL </th>
-                  <th width=8.3%> Email </th>
-                  <th width=8.3%> Phone </th>
-                  <th width=8.3%> Notes </th>
-                  </tr>\n";
-            while($row=mysql_fetch_array($result)){
-                      $commonname=$row['commonname'];
-                      $scientificname=$row['scientificname'];
-                      $name=$row['name'];
-                      $databasecode=$row['databasecode'];
-                      $state=$row['state'];
-                      $zip=$row['zip'];
-                      $url=$row['url'];
-                      $email=$row['email'];
-                      $phone=$row['phone'];
-                      $notes=$row['notes'];
-                      $seed=$row['seed'];
-                      $liveplant=$row['liveplant'];
-              
+              <tr width=80%> 
+              <th width=8.3%> Live Plants Available? </th>
+              <th width=8.3%> Seeds Available? </th>
+              <th width=8.3%> Common Name </th>
+              <th width=8.3%> Scientific Name </th>
+              <th width=8.3%> Plant Database Code </th>
+              <th width=8.3%> Source Name </th>
+              <th width=8.3%> State </th>
+              <th width=8.3%> ZIP </th>
+              <th width=8.3%> URL </th>
+              <th width=8.3%> Email </th>
+              <th width=8.3%> Phone </th>
+              <th width=8.3%> Notes </th>
+              </tr>\n";
+            while(list($commonname, $scientificname, $name, $databasecode, $state, $zip, $url, $email, $phone, $notes, $seed, $liveplant) = $result->fetch_row()){
               echo "
-                  <tr width=80%>
-                  <td width=8.3%> $liveplant </td>
-                  <td width=8.3%> $seed </td>
-                  <td width=8.3%> $commonname </td>
-                  <td width=8.3%> $scientificname </td>
-                  <td width=8.3%> $databasecode </td>
-                  <td width=8.3%> $name </td>
-                  <td width=8.3%> $state </td>
-                  <td width=8.3%> $zip </td>
-                  <td width=8.3%> $url </td>
-                  <td width=8.3%> $email </td>
-                  <td width=8.3%> $phone </td>
-                  <td width=8.3%> $notes </td>
-                  </tr>\n";
+              <tr width=80%>
+              <td width=8.3%> $liveplant </td> 
+              <td width=8.3%> $seed </td> 
+              <td width=8.3%> $commonname </td> 
+              <td width=8.3%> $scientificname </td> 
+              <td width=8.3%> $databasecode </td> 
+              <td width=8.3%> $name </td> 
+              <td width=8.3%> $state </td> 
+              <td width=8.3%> $zip </td> 
+              <td width=8.3%> $url </td> 
+              <td width=8.3%> $email </td> 
+              <td width=8.3%> $phone </td> 
+              <td width=8.3%> $notes </td> 
+              </tr>\n";
             }
             echo "</table>";
-        }
-    }
+          }       
+    }      
     ?>
-</body>
+  </body>
 
 </html>
